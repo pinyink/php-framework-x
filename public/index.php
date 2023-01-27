5e44dbf6bcc50798776aafbfa0cba08ea50658bd
pinyink <nurkhafindi@gmail.com>
2023-01-27 09:22:40 +0700

    add react/mysql

--- public/index.php
+++ public/index.php
@@ -2,6 +2,10 @@
 
 require __DIR__ . '/../vendor/autoload.php';
 // require __DIR__ . '/../framework-x/vendor/autoload.php';
+
+$credentials = ':root@localhost/fm_x?idle=0.001';
+$db = (new React\MySQL\Factory())->createLazyConnection($credentials);
+
 $app = new FrameworkX\App();
 
 $app->get('/', new Acme\Todo\HelloController());
