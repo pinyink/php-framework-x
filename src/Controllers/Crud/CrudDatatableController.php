65d5124cf53686c887ecc4e6a68696a37ff900ab
pinyink <nurkhafindi@gmail.com>
2023-08-03 10:57:49 +0700

    pretify

--- src/Controllers/Crud/CrudDatatableController.php
+++ src/Controllers/Crud/CrudDatatableController.php
@@ -18,45 +18,14 @@ class CrudDatatableController
     public function __invoke(ServerRequestInterface $request) : Response
     {
         $params = $request->getParsedBody();
-        
-        // $coloumSearch = ['user_user', 'user_level'];
-        // $coloumOrder = ['user_user', 'user_level'];
 
-        // $query = 'select * from users';
-        // $queryFiltered = 'select count(*) total from users';
-        // $queryCount = 'select count(*) total from users';
-
-        // // if search active
-        // $paramValue = [];
-        // foreach ($coloumSearch as $kSearch => $vSearch) {
-        //     if (isset($params['search']['value'])) {
-        //         $query .= " WHERE 1=1 ";
-        //         $queryFiltered .= " WHERE 1=1 ";
-        //         foreach ($params['search']['value'] as $key => $value) {
-        //             $query .= "AND {$key} like '%?%' ";
-        //             $queryFiltered .= "AND {$key} like '%?%' ";
-        //             array_push($paramValue, $value);
-        //         }
-        //     }
-        // }
-        // if (isset($params['order'])) {
-        //     $query .= " order by ".$params['order']['0']['column']." ".$params['order']['0']['dir'];
-        // }
-        // if (isset($params['length']) && $params['length'] != -1) {
-        //     $query .= " limit ? offset ?" ;
-        //     array_push($paramValue, $params['length']);
-        //     array_push($paramValue, $params['start']);
-        // }
-        
-        // $result = $this->userModel->query($query, $paramValue);
-        // $filter = $this->userModel->query($queryFiltered, $paramValue);
-        // $count = $this->userModel->query($queryCount, []);
-
+        // load datatable
         $this->userModel->setColumnSearch(['user_user', 'user_level']);
         $this->userModel->setColumnOrder(['user_user', 'user_level']);
         $this->userModel->setQueryDatatable("select * from users");
         $this->userModel->setFilteredDatatable("select count(*) total from users");
         $queryResult = $this->userModel->datatable($params);
+
         $data = [];
         foreach ($queryResult['data'] as $key => $value) {
             $row = array();
