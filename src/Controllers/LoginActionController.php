5b1bbf66dab886ea725ba92ed9a22e65750b5b0a
pinyink <nurkhafindi@gmail.com>
2024-01-25 10:50:39 +0700

    login

--- src/Controllers/LoginActionController.php
+++ src/Controllers/LoginActionController.php
@@ -2,14 +2,31 @@
 
 namespace Acme\Todo\Controllers;
 
+use Acme\Todo\Models\UserModel;
 use Psr\Http\Message\ServerRequestInterface;
 use React\Http\Message\Response;
 
 class LoginActionController
 {
+    private $userModel;
+
+    function __construct(UserModel $userModel)
+    {
+        $this->userModel = $userModel;
+    }
     public function __invoke(ServerRequestInterface $request) : Response
     {
         $data = $request->getParsedBody();
-        return Response::json($data);
+        $query = $this->userModel->findAll(['user_user' => $data['username']], 1);
+        // return Response::json($query);
+        if (empty($query[0])) {
+            return Response::html('<h4>Username Not Found</h4>');
+        }
+        if ($query[0]['user_password'] != null && !password_verify($data['password'], $query[0]['user_password'])) {
+            return Response::html('<h4>Password Not Same</h4>');
+        } else {
+            return Response::json($query);
+        }
+        // return Response::html('<h4>Ok</h4>');
     }
 }
